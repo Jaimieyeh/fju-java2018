5e38510c5babc40c7d317122a15425ba57515583
user <user@es302n28>
2018-10-02 11:00:20 +0800

    exam

---
+++ exam/src/com/fju/Hello/Hello.java
@@ -0,0 +1,16 @@
+package com.fju.Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int age = 18;
+		int height = 160;
+		float weight = 43.5f;
+		
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		
+	}
+
+}
--- myproject/src/com/jaimie/Hello.java
+++ myproject/src/com/jaimie/Hello.java
@@ -3,8 +3,22 @@ package com.jaimie;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		short age = 18;
+		int height = 160;
+		float weight = 43.6f;
+		int schoolYear = 107;
+		String name = "Jaimie Yeh";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		System.out.println(schoolYear);
 		
+		
+		
+		
+		
+				
 		
 
 	}
