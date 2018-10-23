7a47bc5f574ca8175b52cf0f060461f2f87d0bc1
user <user@es302n27>
2018-10-23 12:07:46 +0800

    variables

---
+++ myproject/src/com/jaimie/RangeTester.java
@@ -0,0 +1,20 @@
+package com.jaimie;
+
+import java.util.Scanner;
+
+public class RangeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please enter a number");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		boolean yes = 
+		
+	}
+}
+		
+
+	
+
+
--- myproject/src/com/jaimie/SighUp.java
+++ myproject/src/com/jaimie/SighUp.java
@@ -10,8 +10,21 @@ public class SighUp {
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
 		boolean adult = line.equalsIgnoreCase("Y");
+		if (adult){
+			System.out.println("Your age?");
+			int age = scanner.nextInt();
+			scanner.nextLine();
+			System.out.println("Your name?");
+			String name = scanner.nextLine();
+			System.out.println("Your nick name?");
+			String nickName = scanner.nextLine();
+			System.out.println(age + "/" + name + "/" + nickName);
 		
+		}else{
+			System.out.println("Bye bye~");
 		
 	}
-
+	}
 }
+
+
